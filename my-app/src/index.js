42ac3f13e5e88e9f96a1645dd66171c530e87c57
Numan Khan <numankhan@Numans-MBP.home>
2019-12-24 07:24:57 -0500

    Board maintains state, not each Square--using onClick()

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,18 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    // allows us to store the state of the Square
+    // constructor(props) {    
+    //     super(props);   // parent class is the Board component
+    //     this.state = {
+    //         value : null,
+    //     };
+    // }
+
     render() {
       return (
-        <button className="square">
-          {this.props.value}
+        // When this button is clicked, we re-render given the props
+        // and displays it by having {} within the button tag
+        <button 
+         className="square" 
+         onClick={() => this.props.onClick()} // sets up a click event listener 
+        >
+          {this.props.value}    
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    // storing Game state within the Board
+    constructor(props) {
+        super(props); // parent class is the Game component
+        this.state = {
+            squares : Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        // using slice(), we can avoid mutation
+        const squares = this.state.squares.slice(); // creates a copy of squares 
+        squares[i] = 'X';
+        this.setState({squares : squares});
+    }
+
+    // function that we call to re-render the square with a value
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (
+          <Square 
+           value={this.state.squares[i]}
+           onClick = {() => this.handleClick(i)}
+          />
+          );
     }
   
     render() {

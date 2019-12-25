8db01c22ffafceb5218675bd83b7a4f36ca81bc6
numankh <khannuman15@gmail.com>
2019-12-25 10:40:13 -0500

    created a list of buttons--need to implement jumpTo() & assign keys to dynamic list of buttons

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -114,6 +114,18 @@ import './index.css';
       const currBoard = history[history.length - 1]; // accesses the most recent board in history
       const winner = calculateWinner(currBoard.squares); // check if there's a winner, in curr board
 
+      // list of buttons that map to previous boards
+      const moves = history.map((step,move) => {
+        const desc = move ? 
+          'Go to move #' + move : 
+          'Go to game start';
+         return (
+           <li>
+             <button onClick={() => this.jumpTo(move)}> {desc} </button>
+           </li>
+         )
+      });
+
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
@@ -131,7 +143,7 @@ import './index.css';
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
